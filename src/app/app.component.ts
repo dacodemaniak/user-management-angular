109e8d49c32610315f74996c7cdc18ec0bf93b04
DaCoDeMaNiaK <jla.webprojet@gmail.com>
2024-01-29 11:50:53 +0100

    Structural and attribute directives - Built-in pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'angular-init';
+export class AppComponent implements OnInit, OnDestroy {
+
+  title = 'angular-init'
+
+  public counter: number = 0
+  private counterInterval: any
+
+  public users: Array<any> = [
+    {
+      id: 1,
+      lastname: 'Aubert',
+      firstname: 'Jean-Luc'
+    },
+    {
+      id: 2,
+      lastname: 'Bond',
+      firstname: 'James'
+    },
+    {
+      id: 3,
+      lastname: 'Monde',
+      firstname: 'Ray'
+    }
+  ]
+
+  public showPassword = false
+  private showTimeout: any;
+
+  ngOnInit(): void {
+    this.counterInterval = setInterval(
+      () => this.counter++,
+      1000
+    )
+  }
+
+  ngOnDestroy(): void {
+      clearInterval(this.counterInterval)
+      clearTimeout(this.showTimeout)
+  }
+
+  toggleShowPassword() {
+    this.showPassword = true
+    this.showTimeout = setTimeout(
+      () => this.showPassword = false,
+      1000
+    )
+  }
 }
